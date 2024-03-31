404004baa9cc14c99e9ce2ede7f8a992f625894d
정형진 <88423849+KeMezz@users.noreply.github.com>
2024-03-31 14:24:58 +0900

    remind old way of fetching data in react

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,21 @@
-export const metadata = {
-  title: "Home",
-};
+"use client";
+
+import { useEffect, useState } from "react";
 
 export default function Page() {
-  return <h1>Hello, Next!</h1>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch(
+      "https://nomad-movies.nomadcoders.workers.dev/movies"
+    );
+    const json = await response.json();
+    setMovies(json);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return <p>{isLoading ? "Loading..." : JSON.stringify(movies)}</p>;
 }
