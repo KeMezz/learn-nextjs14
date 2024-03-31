63891e55c10cff74e52caf1fda2fd601ec8463fd
정형진 <88423849+KeMezz@users.noreply.github.com>
2024-03-31 18:58:32 +0900

    server side fetching

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,21 +1,14 @@
-"use client";
+export const metadata = {
+  title: "Home",
+};
 
-import { useEffect, useState } from "react";
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
-export default function Page() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const response = await fetch(
-      "https://nomad-movies.nomadcoders.workers.dev/movies"
-    );
-    const json = await response.json();
-    setMovies(json);
-    setIsLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
+async function getMovies() {
+  return await fetch(URL).then((res) => res.json());
+}
 
-  return <p>{isLoading ? "Loading..." : JSON.stringify(movies)}</p>;
+export default async function HomePage() {
+  const movies = await getMovies();
+  return <div>{JSON.stringify(movies)}</div>;
 }
