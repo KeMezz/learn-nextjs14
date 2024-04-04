19d4c50cd51e40d4a69716453f14da6113c493bc
정형진 <88423849+KeMezz@users.noreply.github.com>
2024-04-04 23:20:51 +0900

    navigate programatically with useRouter

--- components/movie.tsx
+++ components/movie.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import Link from "next/link";
 import styles from "../styles/movie.module.css";
+import { useRouter } from "next/navigation";
 
 interface MovieProps {
   poster_path: string;
@@ -8,9 +11,13 @@ interface MovieProps {
 }
 
 export default function Movie({ poster_path, title, id }: MovieProps) {
+  const router = useRouter();
+  const navigateToMovieDetail = () => {
+    router.push(`/movies/${id}`);
+  };
   return (
     <div className={styles.movie}>
-      <img src={poster_path} alt={title} />
+      <img src={poster_path} alt={title} onClick={navigateToMovieDetail} />
       <Link href={`/movies/${id}`}>{title}</Link>
     </div>
   );
