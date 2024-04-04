760d5138a74ca14410063be8d5f95783f7fb49a2
정형진 <88423849+KeMezz@users.noreply.github.com>
2024-04-04 22:38:55 +0900

    undo showing error

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,10 +1,8 @@
 import { API_URL } from "../app/(home)/page";
 
 async function getMovie(id: string) {
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-  throw new Error("Something went wrong!");
-  // const response = await fetch(`${API_URL}/${id}`);
-  // return await response.json();
+  const response = await fetch(`${API_URL}/${id}`);
+  return await response.json();
 }
 export default async function MovieInfo({ id }: { id: string }) {
   const movie = await getMovie(id);
